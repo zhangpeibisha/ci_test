e28c01390d607058ccfaaf573c067f5bc79a4a64
zhangpei <zhangpe0312@qq.com>
2019-06-11 02:33:22 +0800

    修改脚本

--- bulid.sh
+++ bulid.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 sudo fuser -k -n tcp 8081
-docker stop ceshi/ci_ceshi
+docker stop ci_test_container
+docker rm ci_test_container
 mv ./target/learn.jar .
 docker build -t ceshi/ci_ceshi .
-docker run -d -p 8081:8081 ceshi/ci_ceshi /bin/sh  -c "java -jar /usr/local/web/learn.jar --server.port=8081"
+docker run -d -p 8081:8081 --name ci_test_container ceshi/ci_ceshi /bin/sh  -c "java -jar /usr/local/web/learn.jar --server.port=8081"
 
