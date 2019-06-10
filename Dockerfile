06470003c058aef70a72b3d651e34af6321a8fb1
zhangpei <zhangpe0312@qq.com>
2019-06-11 02:18:06 +0800

    修改脚本

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,6 @@ MAINTAINER zhangpei
 RUN mkdir -p /usr/local/web
 
 COPY ./learn.jar  /usr/local/web/learn.jar
-COPY ./bulid.sh  /usr/local/web/bulid.sh
-COPY ./start.sh  /usr/local/web/start.sh
 
-RUN chmod 777  /usr/local/web/start.sh
-
-CMD bash /usr/local/web/start.sh
 
 
--- bulid.sh
+++ bulid.sh
@@ -4,4 +4,5 @@ sudo fuser -k -n tcp 8081
 docker stop ceshi/ci_ceshi
 mv ./target/learn.jar .
 docker build -t ceshi/ci_ceshi .
-docker run -d -p 8081:8081 ceshi/ci_ceshi
+docker run -d -p 8081:8081 ceshi/ci_ceshi /bin/sh  -c "java -jar /usr/local/web/learn.jar --server.port=8081"
+
