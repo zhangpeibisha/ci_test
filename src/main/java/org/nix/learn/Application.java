d9ae507543562815a463bbf9ba0233cd24af6820
zhangpeibisha <zhangpe0312@qq.com>
2019-06-10 17:52:39 +0800

    web

---
+++ src/main/java/org/nix/learn/Application.java
@@ -0,0 +1,18 @@
+package org.nix.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangpei
+ * @date 2019/6/10 17:39
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+
+}
---
+++ src/main/java/org/nix/learn/web/TestController.java
@@ -0,0 +1,24 @@
+package org.nix.learn.web;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * @author zhangpei
+ * @date 2019/6/10 17:40
+ */
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+
+    @GetMapping(value = "/hello")
+    public String hello() {
+        return StrUtil.format("hello {}", DateUtil.format(new Date(), "yyyy-MM-dd HH:mm:SS"));
+    }
+
+}
