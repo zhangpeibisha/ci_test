0c620cfff1ff60f051a0fb528e8f9e69e2394445
zhangpeibisha <zhangpe0312@qq.com>
2019-06-10 18:00:38 +0800

    修改脚本

--- bulid.sh
+++ bulid.sh
@@ -1,3 +1,3 @@
 sudo fuser -k -n tcp 8082
 sudo fuser -k -n tcp 8081
-nohup java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8082  learn --server.port=8081 --spring.profiles.active=dev&
+nohup java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8082  ./target/learn.jar --server.port=8081 --spring.profiles.active=dev&
