a4699351698b0998c974807b5bf00017dfecb5aa
zhangpei <zhangpe0312@qq.com>
2019-06-11 02:06:03 +0800

    修改脚本

--- start.sh
+++ start.sh
@@ -2,5 +2,4 @@
 
 sudo fuser -k -n tcp 8082
 sudo fuser -k -n tcp 8081
-nohup java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8082 learn.jar --server.port=8081&
-tail -f nohup.out
+java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8082 learn.jar --server.port=8081
