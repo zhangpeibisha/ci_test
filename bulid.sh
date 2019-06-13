aaa9504dcf07c9bf1d100adb9e151d90b7c32f37
zhangpei <Bisha520>
2019-06-14 02:18:27 +0800

    提交CI启动脚本

--- bulid.sh
+++ bulid.sh
@@ -3,6 +3,7 @@
 sudo fuser -k -n tcp 8081
 docker stop ci_test_container
 docker rm ci_test_container
+docker rmi ceshi/ci_ceshi
 mv ./target/learn.jar .
 docker build -t ceshi/ci_ceshi .
 docker run -d -p 8081:8081 --name ci_test_container ceshi/ci_ceshi /bin/sh  -c "java -jar /usr/local/web/learn.jar --server.port=8081"
